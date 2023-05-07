67d69778a540f841146d5527602091a440493ee1
YannyD <yan@wemeta.world>
2023-05-07 14:15:50 -0500

    feat: sdk client integrated

--- packages/web-app/src/index.tsx
+++ packages/web-app/src/index.tsx
@@ -3,11 +3,12 @@ import WalletConnectProvider from '@walletconnect/web3-provider/dist/umd/index.m
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router} from 'react-router-dom';
-import 'tailwindcss/tailwind.css';
 import {UseSignerProvider} from 'context/signer';
+import 'tailwindcss/tailwind.css';
 import {IProviderOptions} from 'web3modal';
 import {loadConnectKit} from '@ledgerhq/connect-kit-loader';
-
+import {Context, ContextParams, Client} from '@aragon/sdk-client';
+import {ethers} from 'ethers';
 import {AlertProvider} from 'context/alert';
 import {client, goerliClient} from 'context/apolloClient';
 import {APMProvider} from 'context/elasticAPM';
@@ -23,6 +24,37 @@ import {infuraApiKey} from 'utils/constants';
 import App from './app';
 import {Auth0Provider} from '@auth0/auth0-react';
 import secret from '../../../secret.json';
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+const contextParams: ContextParams = {
+  // Choose the network you want to use. You can use "goerli" or "mumbai" for testing, "mainnet" for Ethereum.
+  network: 'goerli',
+  // Depending on how you're configuring your wallet, you may want to pass in a `signer` object here.
+  signer: signer,
+  pluginRepoRegistryAddress: '0x970Eb7Dd57c9F0dc4c5a10c06653d1103946b508',
+  // Pass the address of the  `DaoFactory` contract you want to use. You can find it here based on your chain of choice: https://github.com/aragon/core/blob/develop/active_contracts.json
+  daoFactoryAddress: '0x16B6c6674fEf5d29C9a49EA68A19944f5a8471D3',
+  // Choose your Web3 provider: Cloudfare, Infura, Alchemy, etc.
+  web3Providers: ['https://rpc.ankr.com/eth_goerli'],
+  ipfsNodes: [
+    {
+      url: 'https://testing-ipfs-0.aragon.network/api/v0',
+      headers: {'X-API-KEY': ''},
+    },
+  ],
+  // Don't change this line. This is how we connect your app to the Aragon subgraph.
+  graphqlNodes: [
+    {
+      url: 'https://subgraph.satsuma-prod.com/aragon/core-goerli/api',
+    },
+  ],
+};
+
+const context: Context = new Context(contextParams);
+
+export const aragonClient: Client = new Client(context);
+console.log('client', {aragonClient});
 
 const providerOptions: IProviderOptions = {
   walletconnect: {
